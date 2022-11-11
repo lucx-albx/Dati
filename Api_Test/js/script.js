b42e35cd6012b47199dbb1a45922edb33c782c59
lucx-albx <117486221+lucx-albx@users.noreply.github.com>
2022-11-11 19:12:52 +0100

    Post-Prova1

---
+++ Api_Test/js/script.js
@@ -0,0 +1,26 @@
+const URL = "https://lucx-albx.github.io/Dati/Dati.json"
+
+const update ={
+    Nome : "marco",
+    Cognome : "Prova2",
+    Anni : 17,
+    Nascita : "Italia",
+    Lingua : "Italiano"
+}
+
+const options = {
+    method: 'POST',
+    headers: {
+    'Content-Type': 'application/json',
+},
+body: JSON.stringify(update),
+};
+
+const Api =()=>{
+    fetch(URL, options)
+    .then((testo) => testo.json())
+    .then((update)=>{
+        console.log(update)
+
+    })
+}
